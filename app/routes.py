f30fcb24c8f6eba87a627e0df73087540e8d79eb
CosmicTiger <luisangelmarcia@gmail.com>
2021-03-22 01:13:19 -0600

    :construction: - @CosmicTiger - Donde home and base templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,34 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route('/home')
 def index():
-    return "Project 1: TODO"
+    user = { 'username': 'CosmicTiger' }
+    books = [
+        {
+            "title": "Memory",
+            "author": "Doug Lloyd",
+            "year": 2015,
+            "isbn": "1632168146",
+            "review_count": 28,
+            "average_score": 5.0
+        },
+        {
+            "title": "Memory2",
+            "author": "Doug Lloyd",
+            "year": 2015,
+            "isbn": "1632168146",
+            "review_count": 28,
+            "average_score": 5.0
+        },
+        {
+            "title": "Memory3",
+            "author": "Doug Lloyd",
+            "year": 2015,
+            "isbn": "1632168146",
+            "review_count": 28,
+            "average_score": 5.0
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, books=books)
